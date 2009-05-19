0ee5c22781f3f74c9eab8121ca8c0b3c2d73e867
Kim Joar Bekkelund <mail@kimjoar.net>
2009-05-19 21:43:38 +0200

    Updated Fermat. Changed naming in view files according to updates in Fermat.

--- fermat.rb
+++ fermat.rb
@@ -6,10 +6,10 @@ class Fermat
   attr_accessor :cache_path, :images_path, :posts_path, :posts_suffix, :cache_suffix
 
   def initialize
-    @path = File.dirname(__FILE__)
-    @cache_path = @path + "/cache"
-    @images_path = @path + "/images"
-    @posts_path = @path + "/posts"
+    @path         = File.dirname(__FILE__)
+    @cache_path   = File.join(@path, "cache")
+    @images_path  = File.join(@path, "images")
+    @posts_path   = File.join(@path, "posts")
     @posts_suffix = ".markdown"
     @cache_suffix = ".html"
 
@@ -18,14 +18,14 @@ class Fermat
 
   def post(name)
     raise "Name not valid" if name.include?("..")
-    filename = @cache_path + "/" + name + @cache_suffix
+    filename = File.join(@cache_path, name + @cache_suffix)
     raise "File does not exist" unless File.file?(filename) 
     
     File.new(filename).read
   end
 
   def posts
-    filename = @cache_path + "/posts.marshal"
+    filename = File.join(@cache_path, "posts.marshal")
     raise "Posts cache does not exist" unless File.file?(filename)
 
     File.open(filename) do |f|
@@ -39,25 +39,25 @@ class Fermat
 
   def cache?
     Dir.mkdir("cache") unless File.directory?("cache")
-    Dir.glob(@posts_path + "/*" + @posts_suffix).length != Dir.glob(@cache_path + "/*" + @cache_suffix).length
+    Dir.glob(File.join(@posts_path, "*" + @posts_suffix)).length != Dir.glob(File.join(@cache_path, "*" + @cache_suffix)).length
   end
 
   def cache
-    files = Dir.glob(@posts_path + "/*" + @posts_suffix)
+    files = Dir.glob(File.join(@posts_path, "*" + @posts_suffix))
     cached_posts = {}
     
     files.each do |filename|
       post = parse_file(filename)
-      f = File.new(@cache_path + "/" + post.basename + @cache_suffix, "w")
+      f = File.new(File.join(@cache_path, post.slug + @cache_suffix), "w")
       f.flock(File::LOCK_EX)
-      f.write(post.text)
+      f.write(post.body)
       f.flock(File::LOCK_UN)
       f.close
 
       cached_posts[post.date.join("").to_i] = post
     end
 
-    f = File.new(@cache_path + "/posts.marshal", "w")
+    f = File.new(File.join(@cache_path, "posts.marshal"), "w")
     f.flock(File::LOCK_EX)
     f.write(Marshal.dump(cached_posts.sort.reverse.map {|a| a[1]}))
     f.flock(File::LOCK_UN)
@@ -69,20 +69,20 @@ class Fermat
 
     post = Post.new
     base = File.basename(filename, @posts_suffix).split("-", 4)
-    post.basename = base[3]
+    post.slug = base[3]
     post.date = base[0..2]
 
     File.open(filename) do |f|
-      post.heading = f.readline
+      post.title = f.readline
       f.rewind
-      post.text = Maruku.new(f.read).to_html
+      post.body = Maruku.new(f.read).to_html
     end
 
     post
   end
 
   class Post
-    attr_accessor :heading, :text, :basename, :date
+    attr_accessor :title, :body, :slug, :date
   end
 end
 
--- views/rss.builder
+++ views/rss.builder
@@ -7,11 +7,11 @@ xml.rss :version => "2.0" do
 
     @posts.each do |post|
       xml.item do
-        xml.title post.heading
-        xml.link "http://gs.kimjoar.net/post/#{post.basename}"
-        xml.description post.text
+        xml.title post.title
+        xml.link "http://gs.kimjoar.net/post/#{post.slug}"
+        xml.description post.body
         xml.pubDate Time.parse(post.date.to_s).rfc822()
-        xml.guid "http://gs.kimjoar.net/post/#{post.basename}"
+        xml.guid "http://gs.kimjoar.net/post/#{post.slug}"
       end
     end
   end
